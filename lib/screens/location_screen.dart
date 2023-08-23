97de013df11af93f6a27f2962b29ab3a0ac47808
ivannaSevkovychItk <ivanna.sevkovych@itk-engineering.de>
2023-08-23 17:34:09 +0200

    Feat: add fallback data if no weather data obtained

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -25,6 +25,13 @@ class _LocationScreenState extends State<LocationScreen> {
 
   void updateUI(dynamic weatherData) {
     setState(() {
+      if (weatherData == null) {
+        temperature = 0;
+        temperatureIcon = 'Error';
+        weatherIcon = 'Unable to get weather data';
+        cityName = '';
+        return;
+      }
       double temp = weatherData['main']['temp'];
       temperature = temp.toInt();
       temperatureIcon = weather.getMessage(temperature);
@@ -56,7 +63,10 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var weatherData = await weather.getLocationWeather();
+                      updateUI(weatherData);
+                    },
                     child: Icon(
                       Icons.near_me,
                       size: 50.0,

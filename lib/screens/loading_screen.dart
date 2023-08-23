d689f2ef193bb6b56016ebbb585a2883b5b2f58d
ivannaSevkovychItk <ivanna.sevkovych@itk-engineering.de>
2023-08-23 11:08:15 +0200

    Feat: extract networking and location logic to separate classes

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:weather_app/screens/location_screen.dart';
+import 'package:weather_app/services/location.dart';
+import 'package:weather_app/services/networking.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,55 +10,40 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  /// Determine the current position of the device.
-  ///
-  /// When the location services are not enabled or permissions
-  /// are denied the `Future` will return an error.
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
+  double latitude = 0.0;
+  double longitude = 0.0;
 
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
+  Future<void> getLocationData() async {
+    String apiKey = dotenv.env['API_KEY'] ?? 'default_api_key';
+
+    Location location = Location();
+
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+
+    if (apiKey != 'default_api_key') {
+      NetworkHelper networkHelper = NetworkHelper(
+          'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric');
+      var weatherData = await networkHelper.getData();
+
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return LocationScreen();
+      }));
+    } else {
+      print('API key not found in environment variables.');
     }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    // return await Geolocator.getCurrentPosition();
-    Position position = await Geolocator.getCurrentPosition();
-    print('Latitude: ${position.latitude}');
-    print('Longitude: ${position.longitude}');
-    return position;
   }
 
   @override
   void initState() {
     super.initState();
-    _determinePosition();
+    loadData();
+  }
+
+  Future<void> loadData() async {
+    await dotenv.load(fileName: ".env");
+    await getLocationData();
   }
 
   Widget build(BuildContext context) {
